716a2659bc79f5c2637b56e23aecfc9638c3cf42
Daksh Miglani <hello@dak.sh>
2017-05-22 20:43:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var express = require('express'),
+var express = require("express"),
     app = express(),
-    request = require('request'),
-    fs = require('fs')
+    request = require("request"),
+    fs = require("fs");
 
 // middleware
-app.use(express.static('static'))
+app.use(express.static("static"));
 
 app.get('/', (req, res) => {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
     res.send(`<h3>Welcome to Scraper ${ip}</h3>`)
-})
+});
 
 app.get('/scrape', (req, res) => {
     if(req.query.url && req.query.name) {
-      request(req.query.url).pipe(fs.createWriteStream(__dirname + '/static/' + req.query.name))
-      res.send(`<h3>Work has been Completed</h3>`)
+      request(req.query.url).pipe(fs.createWriteStream(__dirname + "/static/" + req.query.name))
+      res.send("<h3>Work has been Completed</h3>")
     } else {
-      res.send('<h3>Please add the valid Query String</h3>')
+      res.send("<h3>Please add the valid Query String</h3>")
     }
-})
+});
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
