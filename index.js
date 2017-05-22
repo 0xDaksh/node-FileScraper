57496043f4662fadf372c84a9000a1339a882383
Daksh Miglani <hello@dak.sh>
2017-05-22 20:44:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ app.use(express.static("static"));
 
 app.get('/', (req, res) => {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
-    res.send(`<h3>Welcome to Scraper ${ip}</h3>`)
+    res.send(`<h3>Welcome to Scraper ${ip}</h3>`);
 });
 
 app.get('/scrape', (req, res) => {
     if(req.query.url && req.query.name) {
-      request(req.query.url).pipe(fs.createWriteStream(__dirname + "/static/" + req.query.name))
-      res.send("<h3>Work has been Completed</h3>")
+      request(req.query.url).pipe(fs.createWriteStream(__dirname + "/static/" + req.query.name));
+      res.send("<h3>Work has been Completed</h3>");
     } else {
-      res.send("<h3>Please add the valid Query String</h3>")
+      res.send("<h3>Please add the valid Query String</h3>");
     }
 });
 
